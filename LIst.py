aae5a59566f04f1c2e1b4018c0234cdea9c377b7
boufdaddy <okoroc333@gmail.com>
2025-04-26 07:48:29 -0600

    created a list

---
+++ LIst.py
@@ -0,0 +1,8 @@
+# Creating a list named backpack with 15 items, including some duplicates
+backpack = [
+    "notebook", "pen", "pencil", "eraser", "ruler", 
+    "laptop", "charger", "water bottle", "snacks", "headphones", 
+    "pen", "notebook", "marker", "sticky notes", "pencil"
+]
+
+print(backpack)
